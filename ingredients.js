1a23d65a339c80d65493072c6dc55c76f13f2e12
danvangogh <djameswhite@yahoo.com>
2018-08-25 19:40:53 +0000

    Add Ingredients While and For loops

---
+++ ingredients.js
@@ -0,0 +1,17 @@
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+// Write a while loop that prints out the contents of ingredients:
+while (ingredients) {
+  console.log(ingredients);
+  break;
+}
+// Write a for loop that prints out the contents of ingredients:
+var i;
+for  (i = 0; i <+ 8; ingredients[++i]); {
+  console.log(ingredients);
+}
+// Write any loop (while or for) that prints out the contents of ingredients backwards:
+var i;
+for  (i = 0; i <+ 8; ingredients[++i]); {
+  console.log(ingredients.reverse());
+}
